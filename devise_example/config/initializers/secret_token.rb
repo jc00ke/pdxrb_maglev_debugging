6f2fc2c0ff18267b68507a19275adb788f29b1e4
jc00ke <jesse@jc00ke.com>
2011-04-05 10:04:03 -0700

    Add example rails app

---
+++ devise_example/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseExample::Application.config.secret_token = 'f066bc8b7a43f1881f346487374818fcd2dcc8f4903051f607d8cd03f5b6c48ffcf27d5eec5914480cc024c48c3af15390cdd3728a9edcc14f99c3bb672b9120'
